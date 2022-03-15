e990d6a98cb8d4e640b35623496ad9bc53bf5c5e
RaghuvanshA <raghuvash.mishra@algoworks.com>
2022-03-15 12:10:26 +0530

    code push

--- src/module/utils/Apis/user.js
+++ src/module/utils/Apis/user.js
@@ -1,17 +1,15 @@
-import axios from 'axios';
-import { endOfDay } from 'date-fns';
-import {APIUrl} from '../api-constants';
+import axios from "axios";
+import { endOfDay } from "date-fns";
+import { APIUrl } from "../api-constants";
 
-export const signInApi = async (enpoint,payload) => {
-
- console.log(`${APIUrl.userbaseUrl}${enpoint}`,payload)
+export const signInApi = async (enpoint, payload) => {
+  console.log(`${APIUrl.userbaseUrl}${enpoint}`, payload);
   return axios({
-    method: 'POST',
-    url:`${APIUrl.userbaseUrl}${enpoint}`,
-    headers:{
-      'x-api-key':'prabhat@cab'
+    method: "POST",
+    url: `${APIUrl.userbaseUrl}${enpoint}`,
+    headers: {
+      "x-api-key": "prabhat@cab",
     },
-    data:payload
+    data: payload,
   });
 };
-
--- src/router/AuthRoutes.js
+++ src/router/AuthRoutes.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { Component } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 const Stack = createStackNavigator();
 import {
-    ConfirmationScreen, LoginScreen, OtpScreen,
-    OtpVerify,RegistrationNext,RegistrationScreen
-} from '../index'
-
+  ConfirmationScreen,
+  LoginScreen,
+  OtpScreen,
+  OtpVerify,
+  RegistrationNext,
+  RegistrationScreen,
+} from "../index";
 
 function authStack() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="confirmationscreen"
-                headerMode={'none'}
-            >
-                <Stack.Screen name="confrimation" component={ConfirmationScreen} />
-                <Stack.Screen name="login" component={LoginScreen} />
-                <Stack.Screen name="otpscreen" component={OtpScreen} />
-                <Stack.Screen name="otpverify" component={OtpVerify} />
-                <Stack.Screen name="registration" component={RegistrationScreen} />
-                <Stack.Screen name="registrationnext" component={RegistrationNext} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="confirmationscreen"
+        headerMode={"none"}
+      >
+        <Stack.Screen name="confrimation" component={ConfirmationScreen} />
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="otpscreen" component={OtpScreen} />
+        <Stack.Screen name="otpverify" component={OtpVerify} />
+        <Stack.Screen name="registration" component={RegistrationScreen} />
+        <Stack.Screen name="registrationnext" component={RegistrationNext} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default authStack;
