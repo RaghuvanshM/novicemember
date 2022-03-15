360594c3a543fc8c267ed11a54edf2b5f86b0645
RaghuvanshA <raghuvash.mishra@algoworks.com>
2022-03-15 10:56:24 +0530

    api integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, {useEffect} from 'react';
-import {Provider} from 'react-redux';
-import {Store} from './module/store'
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistStore} from 'redux-persist';
-import RouterComponent from '../src/router';
-import {StatusBar, View,Alert} from 'react-native';
-import {enableScreens} from 'react-native-screens';
-import SplashScreenComponent from '../src/Screen/Splash';
-import Toast from 'react-native-toast-message';
-import messaging from '@react-native-firebase/messaging';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { Store } from "./module/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import RouterComponent from "../src/router";
+import { StatusBar, View, Alert } from "react-native";
+import { enableScreens } from "react-native-screens";
+import Toast from "react-native-toast-message";
+import messaging from "@react-native-firebase/messaging";
 enableScreens();
 const persistor = persistStore(Store);
 
@@ -18,24 +17,22 @@ const App = () => {
       persistor.flush();
     };
   }, []);
- 
+
   return (
-    <View style={{flex:1}}>
-        <Provider store={Store}>
-          <PersistGate
-            persistor={persistor}
-            children={bootstrapped =>
-              {
-           
-              if (bootstrapped) {
-                return <RouterComponent />;
-              } else {
-                return <SplashScreenComponent />;
-              }
-            }}
-          />
-        </Provider>
-        <Toast ref={(ref) => Toast.setRef(ref)} />
+    <View style={{ flex: 1 }}>
+      <Provider store={Store}>
+        <PersistGate
+          persistor={persistor}
+          children={(bootstrapped) => {
+            if (bootstrapped) {
+              return <RouterComponent />;
+            } else {
+              return null;
+            }
+          }}
+        />
+      </Provider>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
     </View>
   );
 };
--- src/module/saga/usersignup.js
+++ src/module/saga/usersignup.js
@@ -1,44 +1,41 @@
+import { signInApi } from "../utils/Apis/user";
+import { apiFailed, authUser, signupSuccess } from "../actions";
+import { call, put } from "redux-saga/effects";
+import { APIUrl, ENV, ENV_TYPE } from "../utils/api-constants";
+import { SagaIterator } from "redux-saga";
 
-import {signInApi} from '../utils/Apis/user';
-import {apiFailed, authUser, signupSuccess} from '../actions';
-import {call, put} from 'redux-saga/effects';
-import {APIUrl, ENV, ENV_TYPE} from '../utils/api-constants';
-import {SagaIterator} from 'redux-saga';
+import Toast from "react-native-toast-message";
 
-import Toast from 'react-native-toast-message';
-
-export const signUp = function*(action){
-  
+export const signUp = function* (action) {
   try {
-    const userDetails = yield call(signInApi,APIUrl.signup,{...action.payload});
-
-    if(userDetails.data.response.status==='true'){
-    yield put(signupSuccess());
-    Toast.show({
-      type: 'error',
-      text1: userDetails?.data.response.message|| 'Login Success',
-      visibilityTime: 3000,
-      position: 'bottom',
+    const userDetails = yield call(signInApi, APIUrl.signup, {
+      ...action.payload,
     });
-    }else{
+
+    if (userDetails.data.response.status === "true") {
       yield put(signupSuccess());
       Toast.show({
-        type: 'error',
-        text1: userDetails?.data.response.message|| 'Login Success',
+        type: "error",
+        text1: userDetails?.data.response.message || "Login Success",
         visibilityTime: 3000,
-        position: 'bottom',
+        position: "bottom",
       });
-
+    } else {
+      yield put(signupSuccess());
+      Toast.show({
+        type: "error",
+        text1: userDetails?.data.response.message || "Login Success",
+        visibilityTime: 3000,
+        position: "bottom",
+      });
     }
-    
   } catch (e) {
-
     yield put(apiFailed(e?.response?.data?.message));
     Toast.show({
-      type: 'error',
-      text1: e?.response?.data?.message || 'Somthing Went Wrong',
+      type: "error",
+      text1: e?.response?.data?.message || "Somthing Went Wrong",
       visibilityTime: 3000,
-      position: 'bottom',
+      position: "bottom",
     });
   }
 };
